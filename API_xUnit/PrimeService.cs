81eff9a69b12472214dbddd33a564ba761a083b2
Sherry Fan <v-sherryfan@microsoft.com>
2023-04-12 03:34:04 -0700

    vs code

---
+++ API_xUnit/PrimeService.cs
@@ -0,0 +1,53 @@
+﻿namespace API_xUnit
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate < 2)
+            {
+                return false;
+            }
+            for (int divisor = 2; divisor <= Math.Sqrt(candidate); divisor++)
+            {
+                if (candidate % divisor == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+#nullable disable
+            app.Run(async (context) =>
+            {
+                if (context.Request.Path.Value.Contains("checkprime"))
+                {
+                    int numberToCheck;
+                    try
+                    {
+                        numberToCheck = int.Parse(context.Request.QueryString.Value.Replace("?", ""));
+                        var primeService = new PrimeService();
+                        if (primeService.IsPrime(numberToCheck))
+                        {
+                            await context.Response.WriteAsync(numberToCheck + " is prime!");
+                        }
+                        else
+                        {
+                            await context.Response.WriteAsync(numberToCheck + " is NOT prime!");
+                        }
+                    }
+                    catch
+                    {
+                        await context.Response.WriteAsync("Pass in a number to check in the form /checkprime?5");
+                    }
+                }
+                else
+                {
+                    await context.Response.WriteAsync("Hello World! To check if a number is prime, provide URL of the form /checkprime?5");
+                }
+            });
+        }
+    }
+}
---
+++ PrimeWeb1.UnitTests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,26 @@
+﻿using API_xUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeWeb1.UnitTests
+{
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [Fact]
+        public void ReturnFalseGivenValueOf1()
+        {
+            var result = _primeService.IsPrime(1);
+
+            Assert.False(result, $"1 should not be prime");
+        }
+    }
+}
